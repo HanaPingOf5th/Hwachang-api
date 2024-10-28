44ea52d384fbc27ef7fbfe13e8097b2954500501
oU-Ua <imuu30071126@gmail.com>
2024-10-28 16:57:31 +0900

    Initial commit

---
+++ src/main/java/com/hwachang/hwachangapi/HwachangApiApplication.java
@@ -0,0 +1,13 @@
+package com.hwachang.hwachangapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwachangApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwachangApiApplication.class, args);
+    }
+
+}

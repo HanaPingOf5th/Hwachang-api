74faacc5af5e598f87432bb15943b8e61d4e2826
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-12-19 15:52:12 +0900

    feat: RedisQueueTemplate 추가

--- src/main/java/com/hwachang/hwachangapi/utils/config/RedisConfig.java
+++ src/main/java/com/hwachang/hwachangapi/utils/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.hwachang.hwachangapi.utils.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hwachang.hwachangapi.domain.tellerModule.dto.QueueCustomerDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +11,11 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.List;
+import java.util.PriorityQueue;
 
 @EnableCaching
 @Configuration
@@ -52,6 +56,18 @@ public class RedisConfig {
 
         // 값은 JSON 직렬화
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, PriorityQueue<QueueCustomerDto>> redisQueueTemplate() {
+        RedisTemplate<String, PriorityQueue<QueueCustomerDto>> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // PriorityQueue를 직렬화할 때 적절한 Serializer를 설정
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return redisTemplate;
     }
 }

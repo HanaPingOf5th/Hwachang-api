84a6bcb836b31dd7951427fa5d1feed98786bb87
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-12-19 17:39:18 +0900

    fix: 대기열 상세 정보 조회 DTO 수정

--- src/main/java/com/hwachang/hwachangapi/domain/tellerModule/dto/QueueResponseDto.java
+++ src/main/java/com/hwachang/hwachangapi/domain/tellerModule/dto/QueueResponseDto.java
@@ -1,13 +1,14 @@
 package com.hwachang.hwachangapi.domain.tellerModule.dto;
 
-import com.hwachang.hwachangapi.domain.tellerModule.dto.QueueData.WaitingListCategory;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.Map;
 
 @Getter
 @Builder
 public class QueueResponseDto {
-    private Map<Long, WaitingListCategory> waitingListCategory;
+    // 고객대기중, 대기, 통화중, 후처리에 해당하는 고객 및 행원 수
+    private Long waitingCustomer;
+    private Long waitingTeller;
+    private Long calling;
+    private Long postProcessing;
 }

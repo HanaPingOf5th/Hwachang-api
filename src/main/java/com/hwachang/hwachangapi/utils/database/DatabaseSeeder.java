3b6e559e4f1c37da1395e0c8173dc5e06444d3d2
kiminyoung0628 <152356822+kiminyoung0628@users.noreply.github.com>
2024-12-28 15:59:54 +0900

    feat : Databaseseed 행원 추가 및 실제상담 녹음데이터 추가

--- src/main/java/com/hwachang/hwachangapi/utils/database/DatabaseSeeder.java
+++ src/main/java/com/hwachang/hwachangapi/utils/database/DatabaseSeeder.java
@@ -1,7 +1,6 @@
 package com.hwachang.hwachangapi.utils.database;
 
 import com.hwachang.hwachangapi.domain.consultingRoomModule.dto.CategoryDto;
-import com.hwachang.hwachangapi.domain.consultingRoomModule.dto.CreateReviewDto;
 import com.hwachang.hwachangapi.domain.consultingRoomModule.entities.CategoryEntity;
 import com.hwachang.hwachangapi.domain.consultingRoomModule.entities.ConsultingRoomEntity;
 import com.hwachang.hwachangapi.domain.consultingRoomModule.entities.DocumentEntity;
@@ -10,8 +9,6 @@ import com.hwachang.hwachangapi.domain.consultingRoomModule.repository.CategoryR
 import com.hwachang.hwachangapi.domain.consultingRoomModule.repository.ConsultingRoomRepository;
 import com.hwachang.hwachangapi.domain.consultingRoomModule.repository.DocumentRepository;
 import com.hwachang.hwachangapi.domain.consultingRoomModule.service.CategoryService;
-import com.hwachang.hwachangapi.domain.consultingRoomModule.service.ConsultingRoomService;
-import com.hwachang.hwachangapi.domain.consultingRoomModule.service.DocumentService;
 import com.hwachang.hwachangapi.domain.customerModule.dto.CustomerSignupRequestDto;
 import com.hwachang.hwachangapi.domain.customerModule.entities.CustomerEntity;
 import com.hwachang.hwachangapi.domain.customerModule.entities.ReviewEntity;
@@ -56,6 +53,14 @@ public class DatabaseSeeder implements CommandLineRunner {
                 .build();
         this.tellerService.signup(tellerRequestDto);
 
+        CreateTellerRequestDto tellerRequestDto2 = CreateTellerRequestDto.builder()
+                .tellerNumber("sz1234")
+                .name("임수진")
+                .position("대리")
+                .password("12345")
+                .build();
+        this.tellerService.signup(tellerRequestDto2);
+
         CustomerSignupRequestDto customerRequestDto = CustomerSignupRequestDto.builder()
                 .username("dw1234")
                 .name("김동은")
@@ -64,7 +69,9 @@ public class DatabaseSeeder implements CommandLineRunner {
                 .build();
         this.customerService.signup(customerRequestDto);
 
-        TellerEntity tellerEntity = this.tellerRepository.findTellerByUserName("hj1234").orElseThrow();
+        TellerEntity tellerEntity1 = this.tellerRepository.findTellerByUserName("hj1234").orElseThrow();
+        TellerEntity tellerEntity2 = this.tellerRepository.findTellerByUserName("sz1234").orElseThrow();
+
         CustomerEntity customerEntity = this.customerRepository.findByUsername("dw1234").orElseThrow();
 
         // Creating categories
@@ -109,7 +116,7 @@ public class DatabaseSeeder implements CommandLineRunner {
         consultingRoom1Text.add(createTextEntry("00:00:06", "00:00:10", "네, 어떤 부분이 궁금하신가요?", "상담원"));
 
         ConsultingRoomEntity consultingRoom1 = ConsultingRoomEntity.builder()
-                .tellerId(tellerEntity.getId())
+                .tellerId(tellerEntity1.getId())
                 .categoryId(depositCategoryId)
                 .customerIds(Collections.singletonList(customerEntity.getId()))
                 .originalText(consultingRoom1Text)
@@ -128,7 +135,7 @@ public class DatabaseSeeder implements CommandLineRunner {
         consultingRoom2Text.add(createTextEntry("00:00:08", "00:00:15", "네, 적합한 상품을 안내드리겠습니다.", "상담원"));
 
         ConsultingRoomEntity consultingRoom2 = ConsultingRoomEntity.builder()
-                .tellerId(tellerEntity.getId())
+                .tellerId(tellerEntity1.getId())
                 .categoryId(savingsCategoryId)
                 .customerIds(Collections.singletonList(customerEntity.getId()))
                 .originalText(consultingRoom2Text)
@@ -142,20 +149,69 @@ public class DatabaseSeeder implements CommandLineRunner {
                 .time("2024-10-11")
                 .build();
 
+        List<Map<String, Object>> consultingRoom3Text = new ArrayList<>();
+        consultingRoom3Text.add(createTextEntry("00:00:01", "00:00:09", "안녕하세요. 요즘 금리가 높 은 예금 상품이 있다고 들었 는데요. 어떤 상품이 있는지 알려주실 수 있나요?", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:10", "00:00:17", "안녕하세요. 저희 은행에서는 지금 금리가 높은 정기예금 상품으로 스마트 정기 예금 이 있습니다. 기본 금리가 4.5%이고 1년 만기로 운 영됩니다.", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:18", "00:00:25", "조건이 따로 있나요? 가입 최소 금액은 100만 원이고 모바일 앱으로 가입하시면 추가 금액의 0.2% 수익을 받으실 수 있습니다.", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:26", "00:00:33", "모바일 앱 가입이라면 은행에 직접 방문하지 않아도 되나 요? 네 맞습니다. 앱에서 간단히 본인 인증 후 바로 가입하실 수 있습니다.", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:34", "00:00:42", "필요하시면 가입 방법도 안내 해 드릴게요. 좋아요. 안내 부탁드려요.", "발화자1"));
+
+        ConsultingRoomEntity consultingRoom3 = ConsultingRoomEntity.builder()
+                .tellerId(tellerEntity1.getId())
+                .categoryId(savingsCategoryId)
+                .customerIds(Collections.singletonList(customerEntity.getId()))
+                .originalText(consultingRoom3Text)
+                .summary("주요주제 : 금리 높은 예금 상품 문의" +
+                        " - 금리 높은 예금 상품 문의함" +
+                        " - 해당 은행에는 스마트 정기 예금이 있음 (기본금리 4.5%, 1년 만기)" +
+                        " - 가입 최소 금액은 100만원, 모바일 앱으로 가입시 추가금액의 0.2% 수익 가능" +
+                        " - 모바일 앱 가입이므로 은행 방문 불필요하며 간단한 본인 인증 후 가입 가능"
+                )
+                .recordChat(new ArrayList<>())
+                .voiceRecordUrl("https://kr.object.ncloudstorage.com/consulting-audiofile/consulting-data-d9a2f5f4-768a-4ebf-8b6b-445f2250c194.mp4")
+                .time("2024-12-28")
+                .build();
+
+        List<Map<String, Object>> consultingRoom4Text = new ArrayList<>();
+        consultingRoom3Text.add(createTextEntry("00:00:01", "00:00:09", "도는 하고 지금 적금을 알아 보고 있습니다. 추천해 주실 만한 상품이 있을까요?", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:10", "00:00:17", "안녕하세요. 은행의 드림 적 금 상품을 추천드립니다. 월 납입 금액과 기간에 따라 다양한 옵션이 있습니다.", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:18", "00:00:25", "이 상품이 어떻게 되나요? 기본 금리는 3.5%이고 1 년 이상 유지하면 최대 4% 까지 받을 수 있습니다.", "발화자1"));
+        consultingRoom3Text.add(createTextEntry("00:00:26", "00:00:33", "감사합니다.", "발화자1"));
+
+        ConsultingRoomEntity consultingRoom4 = ConsultingRoomEntity.builder()
+                .tellerId(tellerEntity2.getId())
+                .categoryId(savingsCategoryId)
+                .customerIds(Collections.singletonList(customerEntity.getId()))
+                .originalText(consultingRoom4Text)
+                .summary("주요주제 : 금리 높은 예금 상품 문의" +
+                        " - 금리 높은 정기예금 상품 문의" +
+                        " - 해당 상품 설명, 기본금리 4.5%, 1년 만기" +
+                        " - 가입조건 확인, 최소금액 100만원, 모바일앱 가입 시 추가금리 0.2%" +
+                        " - 모바일앱 가입시 은행 방문 불필요함 알림" +
+                        " - 가입방법 안내"
+                )
+                .recordChat(new ArrayList<>())
+                .voiceRecordUrl("https://kr.object.ncloudstorage.com/consulting-audiofile/consulting-data-b2d6d2c8-135f-4b23-b912-14bf39a312b5.mp4")
+                .time("2024-12-28")
+                .build();
+
         consultingRoomRepository.save(consultingRoom1);
         consultingRoomRepository.save(consultingRoom2);
+        consultingRoomRepository.save(consultingRoom3);
+        consultingRoomRepository.save(consultingRoom4);
+
 
         //create review
         ReviewEntity reviewEntity1 = ReviewEntity.builder()
                 .customerId(customerEntity.getId())
-                .tellerId(tellerEntity.getId())
+                .tellerId(tellerEntity1.getId())
                 .nps(10)
                 .content("훌륭함")
                 .consultingRoomId(consultingRoom1.getConsultingRoomId())
                 .build();
         ReviewEntity reviewEntity2 = ReviewEntity.builder()
                 .customerId(customerEntity.getId())
-                .tellerId(tellerEntity.getId())
+                .tellerId(tellerEntity1.getId())
                 .nps(6)
                 .content("부족함")
                 .consultingRoomId(consultingRoom1.getConsultingRoomId())
